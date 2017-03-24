5e1a6b5289748c54cc72b7c67c262b4c7408b9ae
perix2 <pateldivyesh009@gmail.com>
2017-03-24 17:03:04 +0530

    upto menu

--- Alexa Banking/Main.Designer.cs
+++ Alexa Banking/Main.Designer.cs
@@ -33,19 +33,21 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackgroundImage = global::Alexa_Banking.Properties.Resources.menu;
-            this.panel1.Location = new System.Drawing.Point(226, 62);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(155, 117);
             this.panel1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(258, 221);
+            this.button1.Location = new System.Drawing.Point(33, 163);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(84, 46);
             this.button1.TabIndex = 1;
@@ -54,7 +56,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(258, 286);
+            this.button2.Location = new System.Drawing.Point(34, 250);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(83, 50);
             this.button2.TabIndex = 2;
@@ -63,7 +65,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(258, 362);
+            this.button3.Location = new System.Drawing.Point(37, 343);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(80, 46);
             this.button3.TabIndex = 3;
@@ -80,21 +82,30 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(13, 13);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(154, 469);
+            this.panel2.TabIndex = 5;
+            // 
             // current
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(598, 494);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.panel1);
             this.Name = "current";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Welcome";
             this.Load += new System.EventHandler(this.Main_Load);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -106,6 +117,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel2;
 
 
 
